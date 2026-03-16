9051c3f0b6709104a4bfb4feb4af832c89fff043
mayesha6 <soumyrhmn234@gmail.com>
2026-03-16 14:43:04 +0600

    Wallet

--- src/app/modules/wallet/wallet.controller.ts
+++ src/app/modules/wallet/wallet.controller.ts
@@ -3,6 +3,8 @@ import { catchAsync } from "../../utils/catchAsync"
 import { sendResponse } from "../../utils/sendResponse"
 import httpStatus from "http-status-codes";
 import { WalletServices } from "./wallet.services";
+import { Designation } from "../user/user.interface";
+import AppError from "../../errorHelpers/AppError";
 
 const getWallet = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
 
@@ -24,6 +26,10 @@ const getWallet = catchAsync(async (req: Request, res: Response, next: NextFunct
 const distributePoints = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
 
     const { designation, points } = req.body
+
+    if (!Object.values(Designation).includes(designation)) {
+        throw new AppError(httpStatus.BAD_REQUEST, "Invalid designation")
+    }
 
     const result =
         await WalletServices.distributePoints(
