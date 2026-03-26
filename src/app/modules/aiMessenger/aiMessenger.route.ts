5ab882b631808df322c7d16f736ba03865003da0
mayesha6 <soumyrhmn234@gmail.com>
2026-03-26 11:23:21 +0600

    ai

---
+++ src/app/modules/aiMessenger/aiMessenger.route.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import { regenerateSchema } from "./aiMessenger.validation";
+import { AiMessengerController } from "./aiMessenger.controller";
+import { aiRateLimiter } from "../../middlewares/aiRateLimiter";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { validateRequest } from "../../middlewares/validateRequest";
+
+const router = express.Router();
+
+router.post(
+    "/regenerate",
+    checkAuth("USER", "ADMIN", "SUPER_ADMIN"),
+    aiRateLimiter,
+    validateRequest(regenerateSchema),
+    AiMessengerController.regenerate
+);
+
+export const AiMessengerRoutes = router;
