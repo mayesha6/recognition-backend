d6db357f9c687f2accb65bb54efb54a309a96360
mayesha6 <soumyrhmn234@gmail.com>
2026-04-09 07:43:52 +0600

    redis

--- src/app/config/redis.config.ts
+++ src/app/config/redis.config.ts
@@ -2,8 +2,8 @@ import { createClient } from 'redis';
 import { envVars } from './env';
 
 export const redisClient = createClient({
-    username: envVars.REDIS_USERNAME,
-    password: envVars.REDIS_PASSWORD,
+    // username: envVars.REDIS_USERNAME,
+    // password: envVars.REDIS_PASSWORD,
     socket: {
         host: envVars.REDIS_HOST,
         port: Number(envVars.REDIS_PORT)
