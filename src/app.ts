2178e348fafd0e64071c970374eec3bff59a26e0
mayesha6 <soumyrhmn234@gmail.com>
2026-05-06 15:40:12 +0600

    cors

--- src/app.ts
+++ src/app.ts
@@ -26,10 +26,37 @@ app.use(cookieParser())
 app.use(express.json())
 app.set("trust proxy", 1);
 app.use(express.urlencoded({ extended: true }))
-app.use(cors({
-    origin: [envVars.FRONTEND_URL, envVars.FRONTEND_LOCALHOST_URL, envVars.DASHBOARD_URL, envVars.DASHBOARD_LOCALHOST_URL, envVars.FRONTEND_DOMAIN_URL, envVars.DASHBOARD_DOMAIN_URL],
-    credentials: true
-}))
+// app.use(cors({
+//     origin: [envVars.FRONTEND_URL, envVars.FRONTEND_LOCALHOST_URL, envVars.DASHBOARD_URL, envVars.DASHBOARD_LOCALHOST_URL, envVars.FRONTEND_DOMAIN_URL, envVars.DASHBOARD_DOMAIN_URL],
+//     credentials: true
+// }))
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      console.log("Request Origin:", origin);
+
+      const allowedOrigins = [
+        envVars.FRONTEND_URL,
+        envVars.FRONTEND_LOCALHOST_URL,
+        envVars.DASHBOARD_URL,
+        envVars.DASHBOARD_LOCALHOST_URL,
+        envVars.FRONTEND_DOMAIN_URL,
+        envVars.DASHBOARD_DOMAIN_URL,
+      ];
+
+      console.log("Allowed Origins:", allowedOrigins);
+
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        console.log("Blocked by CORS:", origin);
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use("/api/v1", router)
 
