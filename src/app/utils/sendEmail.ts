ee8901096edbded357a25ea606176ac5750b974b
mayesha6 <soumyrhmn234@gmail.com>
2026-05-13 11:38:44 +0600

    auth

--- src/app/utils/sendEmail.ts
+++ src/app/utils/sendEmail.ts
@@ -17,11 +17,6 @@ const transporter = nodemailer.createTransport({
         rejectUnauthorized: false 
     }
 })
-console.log("SMTP_USER:", envVars.EMAIL_SENDER.SMTP_USER);
-console.log("SMTP_PASS:", envVars.EMAIL_SENDER.SMTP_PASS);
-console.log("SMTP_PORT:", envVars.EMAIL_SENDER.SMTP_PORT);
-console.log("SMTP_HOST:", envVars.EMAIL_SENDER.SMTP_HOST);
-console.log("SMTP_PASS2:", JSON.stringify(envVars.EMAIL_SENDER.SMTP_PASS));
 
 interface SendEmailOptions {
     from?: string;
@@ -59,11 +54,7 @@ export const sendEmail = async ({
                 contentType: attachment.contentType
             }))
         })
-        console.log("SMTP_USER1:", envVars.EMAIL_SENDER.SMTP_USER);
-console.log("SMTP_PASS1:", envVars.EMAIL_SENDER.SMTP_PASS);
-console.log("info:", info);
 
-        console.log(`\u2709\uFE0F Email sent to ${to}: ${info.messageId}`);
     } catch (error: any) {
         console.log("email sending error", error.message);
         throw new AppError(401, "Email error")
