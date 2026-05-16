c1123d4a45000efafec2d493ef5c58162b9a3011
mayesha6 <soumyrhmn234@gmail.com>
2026-05-16 10:37:49 +0600

    user

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -12,6 +12,7 @@ const router = Router();
 router.post("/register", UserControllers.createUser);
 router.get(
   "/all-users",
+  checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
   UserControllers.getAllUsers
 );
 router.get("/me", checkAuth(...Object.values(Role)), UserControllers.getMe);
