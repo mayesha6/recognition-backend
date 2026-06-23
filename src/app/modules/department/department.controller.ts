01ac856fae0a492fc84058ecb3b3006b3abc31b3
mayesha6 <soumyrhmn234@gmail.com>
2026-06-23 12:27:54 +0600

    department

--- src/app/modules/department/department.controller.ts
+++ src/app/modules/department/department.controller.ts
@@ -2,56 +2,60 @@ import { Request, Response } from "express";
 import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
 import httpStatus from "http-status-codes";
+import { JwtPayload } from "jsonwebtoken";
 import { DepartmentService } from "./department.services";
 
 const createDepartment = catchAsync(async (req: Request, res: Response) => {
-  const result = await DepartmentService.createDepartment(req.body);
+  const user = req.user as JwtPayload;
+  const result = await DepartmentService.createDepartment(req.body, user);
 
   sendResponse(res, {
     success: true,
     statusCode: httpStatus.CREATED,
-    message: "Department created",
+    message: "Department created successfully",
     data: result,
   });
 });
 
-const getDepartments = catchAsync(async (req:Request, res:Response) => {
-  const result = await DepartmentService.getDepartments();
+const getDepartments = catchAsync(async (req: Request, res: Response) => {
+  const user = req.user as JwtPayload;
+  const result = await DepartmentService.getDepartments(user);
 
   sendResponse(res, {
     success: true,
     statusCode: httpStatus.OK,
-    message: "Departments fetched",
+    message: "Departments fetched successfully",
     data: result,
   });
 });
 
-const updateDepartment = catchAsync(async (req:Request, res:Response) => {
+const updateDepartment = catchAsync(async (req: Request, res: Response) => {
   const id = req.params.id;
+  const user = req.user as JwtPayload;
 
-  const result = await DepartmentService.updateDepartment(id, req.body);
+  const result = await DepartmentService.updateDepartment(id, req.body, user);
 
   sendResponse(res, {
     success: true,
     statusCode: httpStatus.OK,
-    message: "Category updated",
+    message: "Department updated successfully",
     data: result,
   });
 });
 
-const deleteDepartment = catchAsync(async (req:Request, res:Response) => {
+const deleteDepartment = catchAsync(async (req: Request, res: Response) => {
   const id = req.params.id;
+  const user = req.user as JwtPayload;
 
-  await DepartmentService.deleteDepartment(id);
+  await DepartmentService.deleteDepartment(id, user);
 
   sendResponse(res, {
     success: true,
     statusCode: httpStatus.OK,
-    message: "Department deleted",
+    message: "Department deleted successfully",
     data: null,
   });
 });
-
 
 export const DepartmentController = {
   createDepartment,
