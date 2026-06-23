e4066ee8de954856317342324f514bd6395c7094
mayesha6 <soumyrhmn234@gmail.com>
2026-06-23 12:42:10 +0600

    category

--- src/app/modules/category/category.model.ts
+++ src/app/modules/category/category.model.ts
@@ -1,23 +1,32 @@
-import { Schema, model } from "mongoose"
-import { ICategory } from "./category.interface"
+import { Schema, model, Types } from "mongoose";
+import { ICategory } from "./category.interface";
 
-const categorySchema = new Schema<ICategory>({
-  name: {
-    type: String,
-    // enum: Object.values(CategoryName),
-    required: true,
-    unique: true
+const categorySchema = new Schema<ICategory>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    images: [
+      {
+        type: String,
+      },
+    ],
+    organizationId: {
+      type: Types.ObjectId,
+      ref: "User",
+      default: null, // null means global/individual system category
+    },
+    createdBy: {
+      type: Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
+  { timestamps: true }
+);
 
-  images: [
-    {
-      type: String
-    }
-  ]
+// Allow same category names across different organizations
+categorySchema.index({ name: 1, organizationId: 1 }, { unique: true });
 
-}, { timestamps: true })
-
-export const Category = model<ICategory>(
-  "Category",
-  categorySchema
-)
+export const Category = model<ICategory>("Category", categorySchema);
