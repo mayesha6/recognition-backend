7b00ecdc053560c69cda32c581d12c3473d0d35f
mayesha6 <soumyrhmn234@gmail.com>
2026-06-23 14:25:35 +0600

    recognition

--- src/app/modules/recognition/recognition.routes.ts
+++ src/app/modules/recognition/recognition.routes.ts
@@ -1,20 +1,24 @@
-import express from "express"
-import { RecognitionControllers } from "./recognition.controller"
-import { validateRequest } from "../../middlewares/validateRequest"
-import { RecognitionValidation } from "./recognition.validation"
-import { checkAuth } from "../../middlewares/checkAuth"
+// recognition.route.ts
+import express from "express";
+import { RecognitionControllers } from "./recognition.controller";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { RecognitionValidation } from "./recognition.validation";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { Role } from "../user/user.interface"; // Use proper Roles
 
-const router = express.Router()
+const router = express.Router();
 
 router.post(
   "/send",
-  checkAuth("SUPER_ADMIN", "ADMIN", "USER"),
-  validateRequest(
-    RecognitionValidation.sendRecognitionValidation
-  ),
+  checkAuth(Role.SUPER_ADMIN, Role.ORGANIZATION_ADMIN, Role.DEPARTMENT_ADMIN, Role.USER),
+  validateRequest(RecognitionValidation.sendRecognitionValidation),
   RecognitionControllers.sendRecognition
-)
+);
 
-router.get("/history", checkAuth("SUPER_ADMIN", "ADMIN", "USER"), RecognitionControllers.getHistory)
+router.get(
+  "/history", 
+  checkAuth(Role.SUPER_ADMIN, Role.ORGANIZATION_ADMIN, Role.DEPARTMENT_ADMIN, Role.USER), 
+  RecognitionControllers.getHistory
+);
 
-export const RecognitionRoutes = router
+export const RecognitionRoutes = router;
