6f1f62e955b167f64c126edca0365939d13cba40
mayesha6 <soumyrhmn234@gmail.com>
2026-06-25 09:56:21 +0600

    reward

---
+++ src/app/modules/reward/reward.interface.ts
@@ -0,0 +1,22 @@
+import { Types } from "mongoose";
+
+export enum RewardStatus {
+  ACTIVE = "Active",
+  INACTIVE = "Inactive",
+}
+
+export interface IReward {
+  name: string;
+  category: string;
+  points: number;
+  stock: number;
+  status: RewardStatus;
+  description?: string;
+  
+  // 🔥 SaaS Tracking
+  organizationId?: Types.ObjectId | null;
+  createdBy: Types.ObjectId;
+
+  createdAt?: Date;
+  updatedAt?: Date;
+}
