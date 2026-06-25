dd1a91d3513d3f2cf7a9fa997d3842b84bc3a5bc
mayesha6 <soumyrhmn234@gmail.com>
2026-06-25 09:58:38 +0600

    reward

---
+++ src/app/modules/reward/reward.controller.ts
@@ -0,0 +1,64 @@
+import { Request, Response } from "express";
+import httpStatus from "http-status-codes";
+import { JwtPayload } from "jsonwebtoken";
+import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
+import { RewardServices } from "./reward.services";
+
+const createReward = catchAsync(async (req: Request, res: Response) => {
+  const user = req.user as JwtPayload;
+  const result = await RewardServices.createReward(req.body, user);
+
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.CREATED,
+    message: "Reward created successfully",
+    data: result,
+  });
+});
+
+const getAllRewards = catchAsync(async (req: Request, res: Response) => {
+  const user = req.user as JwtPayload;
+  const { data, meta } = await RewardServices.getAllRewards(user, req.query as Record<string, string>);
+
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: "Rewards retrieved successfully",
+    meta,
+    data,
+  });
+});
+
+const updateReward = catchAsync(async (req: Request, res: Response) => {
+  const user = req.user as JwtPayload;
+  const id = req.params.id;
+  const result = await RewardServices.updateReward(id, req.body, user);
+
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: "Reward updated successfully",
+    data: result,
+  });
+});
+
+const deleteReward = catchAsync(async (req: Request, res: Response) => {
+  const user = req.user as JwtPayload;
+  const id = req.params.id;
+  await RewardServices.deleteReward(id, user);
+
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: "Reward deleted successfully",
+    data: null,
+  });
+});
+
+export const RewardControllers = {
+  createReward,
+  getAllRewards,
+  updateReward,
+  deleteReward,
+};
