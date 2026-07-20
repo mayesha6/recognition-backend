a2bab83f28641e741ecfde549e5c1885e653734e
mayesha6 <soumyrhmn234@gmail.com>
2026-07-20 16:46:10 +0600

    reward

--- src/app/modules/reward/reward.validation.ts
+++ src/app/modules/reward/reward.validation.ts
@@ -2,27 +2,23 @@ import { z } from "zod";
 import { RewardStatus } from "./reward.interface";
 
 const createRewardZodSchema = z.object({
-
   name: z.string().min(2, "Reward name must be at least 2 characters"),
   category: z.string().min(2, "Category is required"),
-  points: z.number().min(1, "Points required must be at least 1"),
-  stock: z.number().min(0, "Stock cannot be negative"),
+  points: z.coerce.number().min(1, "Points required must be at least 1"),
+  stock: z.coerce.number().min(0, "Stock cannot be negative"),
   status: z.nativeEnum(RewardStatus).optional(),
   description: z.string().optional(),
   image: z.string().optional(),
-
 });
 
 const updateRewardZodSchema = z.object({
-
   name: z.string().min(2).optional(),
   category: z.string().optional(),
-  points: z.number().min(1).optional(),
-  stock: z.number().min(0).optional(),
+  points: z.coerce.number().min(1).optional(),
+  stock: z.coerce.number().min(0).optional(),
   status: z.nativeEnum(RewardStatus).optional(),
   description: z.string().optional(),
   image: z.string().optional(),
-
 });
 
 export const RewardValidation = {
