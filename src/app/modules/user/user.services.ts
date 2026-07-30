f33fa5c9650c87a6fbab7ec5c465778e3216326d
mayesha6 <soumyrhmn234@gmail.com>
2026-07-30 10:34:54 +0600

    user

--- src/app/modules/user/user.services.ts
+++ src/app/modules/user/user.services.ts
@@ -281,6 +281,7 @@ const getAllUsers = async (
   if (decodedToken.role === Role.SUPER_ADMIN) {
     if (query.organizationId === "null") {
       filter.organizationId = null;
+      filter.accountType = AccountType.INDIVIDUAL;
       // For global user management, super admin only wants to see SUPER_ADMIN and USER roles by default
       if (!query.role) {
         filter.role = { $in: [Role.SUPER_ADMIN, Role.USER] };
